821b6ab0bc76afaa5fae0a1171abec34114af745
此昵称不存在 <878740953@qq.com>
2020-09-28 21:33:42 +0800

    完成简单的权限控制，注解方式

--- demo_ssm_web/src/main/java/com/ssm/controller/ProductController.java
+++ demo_ssm_web/src/main/java/com/ssm/controller/ProductController.java
@@ -4,11 +4,14 @@ import com.github.pagehelper.PageInfo;
 import com.ssm.domain.Product;
 import com.ssm.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @Controller
@@ -18,6 +21,10 @@ public class ProductController {
     private ProductService productService;
 
     @RequestMapping("/findAll")
+    // jsr250
+//    @RolesAllowed("ADMIN")
+    @Secured("ROLE_ADMIN")
+//    @PreAuthorize("hasRole(ADMIN)")
     public String finAll(Model model, @RequestParam(required = true, defaultValue = "1") int pageNum, @RequestParam(required = true, defaultValue = "5") int pageSize) throws Exception {
         List<Product> products = productService.finAll(pageNum, pageSize);
         PageInfo productList = new PageInfo(products);
@@ -25,6 +32,8 @@ public class ProductController {
         return "product-list";
     }
     @RequestMapping("/save")
+    // 未生效，没有解决
+//    @PreAuthorize("authentication.principal.username == 'administrator'")
     public String save(Product product) throws Exception {
         System.out.println(product);
         productService.save(product);
